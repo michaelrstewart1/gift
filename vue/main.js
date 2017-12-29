d5d08d025b650e7f46b4938584adafc22d64c606
Michael Stewart <michaelrstewart1@gmail.com>
2017-12-29 12:13:56 -0600

    pushhh

--- vue/main.js
+++ vue/main.js
@@ -1,5 +1,5 @@
 // entry point file
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from '../js/external/vue.esm.js'
 import App from '../vue/components/Main/Main'
 
 new Vue(App).$mount('#app');
