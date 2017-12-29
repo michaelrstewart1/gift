6bb11703e25e3a59c23b4f4b5d9a7923a1a00108
Michael Stewart <michaelrstewart1@gmail.com>
2017-12-29 12:48:06 -0600

    more organization

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Gift</title>
-		<link rel="stylesheet" type="text/css" href="dist/app.css">
+
+		<!-- STYLING -->
+		<link rel="stylesheet" href="dist/tether.css">
+		<link rel="stylesheet" href="dist/bootstrap.css">
+		<link rel="stylesheet" href="dist/app.css">
+
 		<!-- JAVASCRIPT -->
 		<script type="text/javascript" language="javascript" src="js/external/jquery.dev.min.js"></script>
 		<!-- <script type="text/javascript" language="javascript" src="js/external/jquery.prod.min.js"></script> -->
+		<script type="text/javascript" language="javascript" src="js/tether/tether.js"></script>
 		<script type="text/javascript" language="javascript" src="js/external/vue.dev.min.js"></script>
 		<!-- <script type="text/javascript" language="javascript" src="js/external/vue.prod.min.js"></script> -->
+		<script type="text/javascript" language="javascript" src="js/bootstrap/bootstrap.js"></script>
 	</head>
 	<body>
 		<div id="app"></div>
-		<!-- built files will be auto injected -->
 		<script src="dist/vue_modules.js"></script>
 		<script type='text/javascript'>
 			$(document).ready(function() {
